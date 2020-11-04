7b18d3292f11fc92f427e9ac234da85cdac948ca
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 17:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -158,8 +158,25 @@ function moveDodger(e) {
       moveDodgerRight()
     }
   })
-   e.preventDefault()
-   e.stopPropagation()
+
+  window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Should do nothing if the default action has been cancelled
+    }
+  
+    var handled = false;
+    if (event.key !== undefined) {
+      // Handle the event with KeyboardEvent.key and set handled true.
+    } else if (event.keyCode !== undefined) {
+      // Handle the event with KeyboardEvent.keyCode and set handled true.
+    }
+  
+    if (handled) {
+      // Suppress "double action" if event handled
+      event.preventDefault();
+    }
+  }, true);
+
 }
 
 function moveDodgerLeft() {
