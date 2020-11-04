d387f2bd0d3477e5dc69f7e663ec4215df98ac9b
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 18:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   var leftNumbers = dodger.style.left.replace('px', '')
+   var leftNumbers = dodger.style.right.replace('px', '')
  var left = parseInt(leftNumbers, 10)
 
  if (left > 0) {
