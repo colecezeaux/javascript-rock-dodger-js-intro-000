1d762f1f43909bf34c7df28bdfef6abaa4a13283
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 17:11:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    document.addEventListener('keydown', function(e) {
-     if (e.which === LEFT_ARROW) {
+     if (LEFT_ARROW) {
        moveDodgerLeft()
      }
  })
