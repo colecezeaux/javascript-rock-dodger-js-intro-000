0151d404da1c3be9a42bcc7d858cb7e97213fdb6
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 17:02:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -151,21 +151,16 @@ function moveDodger(e) {
    document.addEventListener('keydown', function(e) {
      if (e.which === 37) {
        moveDodgerLeft()
-     } else if (e.which !== 37) {
-       e.preventDefault()
-       e.stopPropagation()
-     }
+     } 
  })
 
   document.addEventListener('keydown', function(e) {
     if (e.which === 39) {
       moveDodgerRight()
-    } else if (e.which !== 39) {
-      e.preventDefault()
-      e.stopPropagation()
-    }
+    } 
   })
-
+   e.preventDefault()
+   e.stopPropagation()
 }
 
 function moveDodgerLeft() {
