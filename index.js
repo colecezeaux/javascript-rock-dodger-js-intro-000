2eb2086fc46b3ab3a9bd565921a7955e5379a688
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 17:17:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ function createRock(x) {
   var top = 0
 
   rock.style.top = top
-  $("#GAME").append(".rock")
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
