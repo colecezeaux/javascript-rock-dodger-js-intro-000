8a9e098c951fa0be7b50a5592deb18db120d62df
Cole Cezeaux <colecezeaux@gmail.com>
2020-11-04 17:50:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -150,35 +150,21 @@ function moveDodger(e) {
    document.addEventListener('keydown', function(e) {
      if (e.which === 37) {
        moveDodgerLeft()
+     } else {
+       e.preventDefault
+       e.stopPropagation
      }
  })
 
   document.addEventListener('keydown', function(e) {
     if (e.which === 39) {
       moveDodgerRight()
+    } else {
+      e.preventDefault
+      e.stopPropagation
     }
   })
 
-  document.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-      return; // Should do nothing if the default action has been cancelled
-    }
-
-    var handled = false;
-    if (event.key !== undefined) {
-      // Handle the event with KeyboardEvent.key and set handled true.
-    } else if (event.keyCode !== undefined) {
-      // Handle the event with KeyboardEvent.keyCode and set handled true.
-    }
-
-    if (handled) {
-      // Suppress "double action" if event handled
-      event.preventDefault();
-    }
-  }, true);
-
-  e.preventDefault
-  e.stopPropagation
 }
 
 function moveDodgerLeft() {
